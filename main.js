5fc94e6e6b666429e6b89b0757c295efb98e9fd1
luisavaldesg <luisa.valdes@siesa.com>
2022-12-18 20:53:30 -0500

    Interacción entre todos los componentees

--- main.js
+++ main.js
@@ -3,40 +3,67 @@ const menuOptions = document.querySelector("#menuOptions");
 const burgerMenu = document.querySelector("#imgMenu");
 const mobileOptions = document.querySelector(".mobileOptions");
 const cartShopping = document.querySelector("#cartShopping");
-const asaidProductDetail = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
+const productDetailContainer = document.querySelector("#productDetail");
+const btnCloseProductDetail = document.querySelector(".product-detail-close");
 
 email.addEventListener("click", toggleMenuDesktop);
 burgerMenu.addEventListener("click", toggleMenuMobile);
 cartShopping.addEventListener("click", toggleOrderAside);
+btnCloseProductDetail.addEventListener("click", closeProductDetailAside);    
 
 function toggleMenuDesktop() {
-  // console.log(menuOptions);
+  console.log(menuOptions);
   // menuOptions.classList.remove('menu');
-  const isOrderMenuClosed = asaidProductDetail.classList.contains("inactive");
+  const isOrderMenuClosed = shoppingCartContainer.classList.contains("inactive");
   if (!isOrderMenuClosed) {
-    asaidProductDetail.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
   menuOptions.classList.toggle("inactive");
 }
+
+
 function toggleMenuMobile() {
   //When I click on the menu mobile then the menu order is hidden
-  const isOrderMenuClosed = asaidProductDetail.classList.contains("inactive");
+  const isOrderMenuClosed = shoppingCartContainer.classList.contains("inactive");
   // menuOptions.classList.remove('menu');
   //imgMenu.classList.toggle('inactive');
   if (!isOrderMenuClosed) {
-    asaidProductDetail.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
+  closeProductDetailAside();
   mobileOptions.classList.toggle("inactive");
 }
+
+
 function toggleOrderAside() {
   //When I click on the the menu order then menu mobile is hidden
   const isMobileMenuClosed = mobileOptions.classList.contains("inactive");
-
   if (!isMobileMenuClosed) {
     mobileOptions.classList.add("inactive");
+
   }
-  asaidProductDetail.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
+
+    //If I click on the shopping cart and Product detail is open then this will be closed.
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive"); 
+    }
+
+
+  
+}
+function closeProductDetailAside(){
+
+  productDetailContainer.classList.add("inactive");
+}
+
+function openProductDetailAside(){
+  //If shopping cart is open and I click on the product detail then shopping cart must be closed.
+  shoppingCartContainer.classList.add("inactive");	
+  productDetailContainer.classList.remove("inactive");
 }
 
 /**shopping cart */
@@ -44,53 +71,59 @@ function toggleOrderAside() {
 // const productList =[];
 
 const productList = [];
-productList.push({
-  name: "Bike",
-  price: 120,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-productList.push({
-  name: "Pantalla",
-  price: 220,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-productList.push({
-  name: "Compu",
-  price: 620,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-productList.push({
-  name: "Bike",
-  price: 120,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-
-productList.push({
-  name: "Motorcycle",
-  price: 300,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-
-productList.push({
-  name: "Car",
-  price: 1000,
-  image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-
-productList.push({
+  productList.push({
+    name: "Bike",
+    price: 120,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
+    name: "Pantalla",
+    price: 220,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
+    name: "Compu",
+    price: 620,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
+    name: "Bike",
+    price: 120,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
+    name: "Motorcycle",
+    price: 300,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
+    name: "Car",
+    price: 1000,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+  productList.push({
   name: "Airplane",
   price: 1200000,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  });
+
+productList.push({
+  name: "Intel Core i7",
+  price: 30000,
   image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 
 renderProducts(productList);
+
+
 
 function renderProducts(arr) {
   console.log(cardsContainer);
@@ -102,6 +135,13 @@ function renderProducts(arr) {
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.image);
 
+    btnCloseProductDetail
+   // productImg.addEventListener("click", toggleMenuDesktop);
+    productImg.addEventListener("click", openProductDetailAside);
+
+    // if(!productImg.classList.contains("inactive")){
+    //   btnCloseProductDetail
+    // }
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
 
